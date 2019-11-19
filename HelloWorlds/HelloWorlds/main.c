b63118379087286ca2e8b886e147cc69dd2fab02
Aman Bhardwaj <amanbhardwaj.04091997@gmail.com>
2019-11-19 05:50:24 +0530

    Hello Friend

---
+++ HelloWorlds/HelloWorlds/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  HelloWorlds
+//
+//  Created by Aman Bhardwaj on 11/19/19.
+//  Copyright © 2019 Projects. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf(" hello worlds! ");
+    return 0;
+}
